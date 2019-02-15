63073f4f484de62c7363bfcdd61fe4224034a3b7
Sadia Mahfuz <mahfuzs@amazon.com>
2019-02-15 10:57:13 +0000

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header';
-import MainMenu from './components/MainMenu';
 
 class App extends Component {
 
