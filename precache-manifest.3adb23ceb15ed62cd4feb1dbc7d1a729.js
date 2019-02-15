068413bb3bf8961d9016b2d886eae5ac69d66a13
Sadia Mahfuz <mahfuzs@amazon.com>
2019-02-15 22:49:17 +0000

    Updates

---
+++ precache-manifest.3adb23ceb15ed62cd4feb1dbc7d1a729.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d90122c3bef773646e5785a21ecdf691",
+    "url": "/DigioWebsite/static/media/transparent.d90122c3.png"
+  },
+  {
+    "revision": "fcc795f6ac87d270fe2e",
+    "url": "/DigioWebsite/static/js/runtime~main.fcc795f6.js"
+  },
+  {
+    "revision": "711fd6a32f5477855d9e",
+    "url": "/DigioWebsite/static/js/main.711fd6a3.chunk.js"
+  },
+  {
+    "revision": "8b9f0481004f2401aee6",
+    "url": "/DigioWebsite/static/js/1.8b9f0481.chunk.js"
+  },
+  {
+    "revision": "711fd6a32f5477855d9e",
+    "url": "/DigioWebsite/static/css/main.aaeaf111.chunk.css"
+  },
+  {
+    "revision": "b8e76329d338524c7728f64105f6bc86",
+    "url": "/DigioWebsite/index.html"
+  }
+];
