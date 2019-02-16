7a47a548a97ac807dc420fdaed506fc4eb5f9874
Syeda Sadia Mahfuz <sadia.mahfuz@gmail.com>
2019-02-16 18:59:04 +0000

    add react-responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import MenuItem from './MenuItem';
+import MediaQuery from 'react-responsive';
 
 const navBarStyle = {
 	alignSelf: 'flex-start'
@@ -17,8 +18,15 @@ export const NavBar = (props) => {
 	const navItemsMapped = navItems.map(item => <MenuItem text={item.text} path={item.path}/>);
 
 	return (
-		<div className="navBar" style={navBarStyle}>
-			{navItemsMapped}
+		<div>
+		<MediaQuery minDeviceWidth={768}>
+			<div className="navBar" style={navBarStyle}>
+				{navItemsMapped}
+				</div>
+		</MediaQuery>
+		<MediaQuery maxWidth={768}>
+        <div>Ok you are on a small screen</div>
+    </MediaQuery>
 		</div>
 	);
 };
