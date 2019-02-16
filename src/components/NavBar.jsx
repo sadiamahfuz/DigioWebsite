a16b8ec08082ad9c39adb568864162d0dae92aa3
Syeda Sadia Mahfuz <sadia.mahfuz@gmail.com>
2019-02-16 21:06:41 +0000

    Revert "add react-responsive"
    This reverts commit 7a47a548a97ac807dc420fdaed506fc4eb5f9874.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import MenuItem from './MenuItem';
-import MediaQuery from 'react-responsive';
 
 const navBarStyle = {
 	alignSelf: 'flex-start'
@@ -18,15 +17,8 @@ export const NavBar = (props) => {
 	const navItemsMapped = navItems.map(item => <MenuItem text={item.text} path={item.path}/>);
 
 	return (
-		<div>
-		<MediaQuery minDeviceWidth={768}>
-			<div className="navBar" style={navBarStyle}>
-				{navItemsMapped}
-				</div>
-		</MediaQuery>
-		<MediaQuery maxWidth={768}>
-        <div>Ok you are on a small screen</div>
-    </MediaQuery>
+		<div className="navBar" style={navBarStyle}>
+			{navItemsMapped}
 		</div>
 	);
 };
