8dd20a93c87d354c01d547f68cc1924c4d4861bf
Syeda Sadia Mahfuz <sadia.mahfuz@gmail.com>
2019-02-23 17:22:35 +0000

    add dummy text to homepage

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -3,12 +3,24 @@ import { pageStyle } from '../styles/commonStyles';
 import Typing from 'react-typing-animation';
 
 const HomePage = (props) => {
+	const homePageText = "Lorem ipsum dolor sit amet, an iusto incorrupte ius, putent incorrupte mea eu, ne reque dissentiet vix. \
+	Ei hinc nihil delectus quo, cum at malis dicat intellegebat, dicta aperiam accumsan at nec. \
+	Tractatos ocurreret ex eum. Sed idque postea postulant ut. Velit commune duo cu, pro lorem cetero temporibus eu. \
+	Utinam civibus voluptua id per. \
+	Aliquip meliore ex vix, ex nusquam epicurei liberavisse est. \
+	Pro cu vero nullam praesent, vim in fabulas nonumes apeirian, in vel dicta phaedrum. \
+	Habemus corpora reprimique ad sit, mel ut minim sadipscing, vitae iudicabit eos eu. \
+	Inani omnium quo in, et quem congue vel."
+
 	return (
 		<div className='homePage' style={pageStyle}>
 			<Typing>
 				<h1> Hello, </h1>
 				<h1> Welcome to Digio! </h1>
 			</Typing>
+			<p>
+				{homePageText}
+			</p>
 		</div>
 	);
 };
