8dde5cf6bb267412ef2185599e1e483165231659
Syeda Sadia Mahfuz <sadia.mahfuz@gmail.com>
2019-03-10 12:38:44 +0000

    populate about us page

--- src/components/pages/AboutUs.jsx
+++ src/components/pages/AboutUs.jsx
@@ -7,7 +7,11 @@ const AboutUs = (props) => {
 	return (
 		<div className='AboutUs' style={pageStyle}>
 			<PageTitle title='Tag line should go here' />
-			<p> History and values </p>
+			<p> Something about our vision </p>
+			<PageTitle title='Who We Are' />
+			<p> Something about the team </p>
+			<PageTitle title='Our Values' />
+			<p> List of values </p>
 		</div>
 	);
 };
