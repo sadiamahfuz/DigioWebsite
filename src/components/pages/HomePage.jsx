011ea0ab3704dba491f4c1741a4404fd620656b6
Syeda Sadia Mahfuz <sadia.mahfuz@gmail.com>
2019-03-13 11:16:58 +0000

    navigate to contact us page from button

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { pageStyle } from '../styles/commonStyles';
 import Typing from 'react-typing-animation';
 import Button from '@material-ui/core/Button';
 import image from '../../images/homePageImage.jpg';
+import { withRouter } from 'react-router-dom';
 
 const Image = (props) => {
 	const imageStyle = {
@@ -17,34 +18,37 @@ const Image = (props) => {
   );
 };
 
-const contactUsClicked = () => {
-	console.log("clicked");
-};
-
-const HomePage = (props) => {
+export class HomePage extends Component {
 	// eslint-disable-next-line
-	const homePageText = "We are a creative digital agency based in London, and are commited to helping you with your projects so you can focus on the things that are important to you. \
+	homePageText = "We are a creative digital agency based in London, and are commited to helping you with your projects so you can focus on the things that are important to you. \
 	We have expertise in everything digital from branding and marketing to product development (software & hardware). \
 	We specialise in:Digital branding & marketing content development (including digital strategy). - Digital product development (software and hardware) (i.e websites, apps, IoT devices etc).\
 	Please get it touch to find out more and how we can help!"
 
-	return (
-		<div>
-			<div className='homePage' style={pageStyle}>
-				<Typing>
-					<h1> Hello, </h1>
-					<h1> Welcome to Digio! </h1>
-				</Typing>
-				<p>
-					{homePageText}
-				</p>
-				<Button variant="contained" color="primary" onClick={contactUsClicked}>
-      		CONTACT US
-    		</Button>
-				<Image />
+	contactUsClicked = () => {
+		this.props.history.push("/contactus")
+	};
+
+	render() {
+		return (
+			<div>
+				<div className='homePage' style={pageStyle}>
+					<Typing>
+						<h1> Hello, </h1>
+						<h1> Welcome to Digio! </h1>
+					</Typing>
+					<p>
+						{this.homePageText}
+					</p>
+					<Button variant="contained" color="primary" onClick={this.contactUsClicked}>
+	      		CONTACT US
+	    		</Button>
+					<Image />
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
+
 };
 
-export default HomePage;
+export default withRouter(HomePage);
