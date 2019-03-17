2fafc0dd58eb8a968105e5362315340f267116a2
Syeda Sadia Mahfuz <sadia.mahfuz@gmail.com>
2019-03-17 19:58:44 +0000

    cut down our work text

--- src/components/pages/OurWork.jsx
+++ src/components/pages/OurWork.jsx
@@ -10,20 +10,20 @@ import contentImage from '../../images/content.jpg';
 import social from '../../images/social.png';
 import softwareImage from '../../images/software.png';
 
-const webAppText = "We're dedicated working with the latest technologies and trends so that your app is fast, stylish, scalable and works across all browsers and devices. \
+const webAppText = "We're dedicated to building fast, stylish, scalable websites and applications that work across all browsers and devices. \
 		Our current favourite technologies are JavaScript based frameworks such as REACT and Node.js.";
 
-const software = "Whatever your software requirements, we have a solution. We have years of experience in building applications to serve various industries, and our team is expert in the modern langauges and framworks including Java, Python, and C#. Just tell us your requirements and we will deliver the solution that is just right for you."
+const software = "Whatever your software requirements, we have a solution. We are experts in all the modern langauges including Java, Python, and C#. Just tell us your requirements and we will deliver a solution that is just right for you."
 
-const machineLearning = "Our team includes experts in the Data Science and Machine Learning. We have expertise in the latest and upcoming machine learning techniques in the industry such as deep learning. Get in touch to talk about your project and requirements.";
+const machineLearning = "We have Data Science and Machine Learning experts who're to date with the latest and greatest in the field, and are active members in the London tech scene. Get in touch to talk about your project and requirements.";
 
-const hardwarePrototyping = "Whether it's creating chatbots or programming microcontrollers, we have the expertise to help. We have years of experience with high performance and low level C coding as well as developing rapid proof of concepts like chatbots.";
+const hardwarePrototyping = "Need to create chatbots, programme microcontrollers or program IoT devices? We have years of experience with high performance and low level C coding as well as developing rapid proof of concepts.";
 
-const ads = "We will deliver high impact and targeted ads on your favourite social media platforms, such as Instagram, Facebook and Snapchat.";
+const ads = "We will deliver high impact and targeted ads on your favourite social media platforms, such as Instagram, Facebook Snapchat and LinkedIn.";
 
-const seo = "We will perform optimisations so that your product and business ranks as high as possible in search engines and widen your reach.";
+const seo = "We will perform search engine optimisations so that your product and business ranks as high as possible in search results and widen your reach.";
 
-const contentCreation = "We will develop and implement a tailored content creation strategy so that you can consistently reach the right audience and derive maximum value from your product.";
+const contentCreation = "We will develop and implement a tailored content creation strategy so that you can consistently reach and appeal to the right audience and derive maximum value.";
 
 const whatWeDoBoxStyle = {
 	maxWidth: 400
